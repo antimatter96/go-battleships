429088ca8b5e7479f7a638fe8483be03ddd3b77a
Arpit Jain <antimatter96@users.noreply.github.com>
2020-08-09 19:39:38 +0530

    Delete stuff

--- main.go
+++ main.go
@@ -18,40 +18,7 @@ type MyCustomClaims struct {
 }
 
 func main() {
-
 	privateKeyPEM, err := ioutil.ReadFile("./private_key.pem")
-
-	//_privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privateKeyPEM)
-	// //fmt.Println(privateKey)
-
-	// tokenString, err := token.SignedString(privateKeyPEM)
-
-	// fmt.Println(">>", tokenString, "<<", err)
-
-	// token2, err := jwt.ParseWithClaims(tokenString, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
-	// 	return privateKeyPEM, nil
-	// })
-
-	// if claims, ok := token2.Claims.(*MyCustomClaims); ok && token2.Valid {
-	// 	fmt.Printf("%+v\n", claims)
-	// } else {
-	// 	fmt.Println(err)
-	// }
-
-	// if token2.Valid {
-	// 	fmt.Println(token2.Claims)
-	// } else if ve, ok := err.(*jwt.ValidationError); ok {
-	// 	if ve.Errors&jwt.ValidationErrorMalformed != 0 {
-	// 		fmt.Println("That's not even a token")
-	// 	} else if ve.Errors&(jwt.ValidationErrorExpired|jwt.ValidationErrorNotValidYet) != 0 {
-	// 		// Token is either expired or not active yet
-	// 		fmt.Println("Timing is everything")
-	// 	} else {
-	// 		fmt.Println("Couldn't handle this token:", err)
-	// 	}
-	// } else {
-	// 	fmt.Println("Couldn't handle this token:", err)
-	// }
 
 	server, err := socketio.NewServer(nil)
 	if err != nil {
